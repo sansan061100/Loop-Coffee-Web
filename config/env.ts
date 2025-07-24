5cab300ab486dcc15248e200ecac393b45840f93
Harithya Wisesa <harithya77@gmail.com>
2025-07-24 13:04:04 +0700

    change api

--- config/env.ts
+++ config/env.ts
@@ -1,2 +1,2 @@
-export const MAP_KEY = "AIzaSyDRWB_TLW_G2mqVb4oAUYb1TyvzKldHA5Y"
+export const MAP_KEY = "AIzaSyCtI8pw0Eq-7PsAosuNX21Xk-Xx6mIyO3A"
 export const GOOGLE_CLIENT_ID = '818840623868-e0tj79a60rvacctarsc5tpq89700dnri.apps.googleusercontent.com'
