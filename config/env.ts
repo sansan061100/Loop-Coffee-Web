9e241dd329c9ab4f76e321b1f07092bba9589f2c
sansan061100 <sansan001106@gmail.com>
2025-09-09 10:53:53 +0700

    ubah google client id

--- config/env.ts
+++ config/env.ts
@@ -1,2 +1,2 @@
 export const MAP_KEY = "AIzaSyCtI8pw0Eq-7PsAosuNX21Xk-Xx6mIyO3A"
-export const GOOGLE_CLIENT_ID = '818840623868-e0tj79a60rvacctarsc5tpq89700dnri.apps.googleusercontent.com'
+export const GOOGLE_CLIENT_ID = '274022457497-55ok8rkr048acg8lldidfo3dlo5gus04.apps.googleusercontent.com'
